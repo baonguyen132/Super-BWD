e0ae9faad5babb8ee4d6ce78a67daa367ad65f2c
Bảo Nguyên <118587919+baonguyen132@users.noreply.github.com>
2025-07-29 21:01:52 +0700

    upload dashboard

--- bae/src/page/Dashboard/dashboard.jsx
+++ bae/src/page/Dashboard/dashboard.jsx
@@ -1,9 +1,22 @@
-function Dashboard({user , dispatch} ) {
-    return ( 
-        <>
-            {user}
-        </>
-     );
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import styles from "./dashboard.module.scss"
+import NavigationCustome from "../../layout/navigation/Navigation.jsx"
+import { Outlet } from "react-router-dom";
+
+function Dashboard() {
+  const { user, dispatch } = useContext(UserContext);
+
+  return (
+    <div className={styles.dashboard_page}>
+      <div className={styles.navigation}>
+        <NavigationCustome />
+      </div>
+      <div className={styles.content}>
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
