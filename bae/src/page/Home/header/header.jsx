b21265e841bbe7e264b12b8493fe98960f5c1710
Bình Nguyễn <134196134+BBCrank3107@users.noreply.github.com>
2025-09-09 22:22:33 +0700

    Update Message

--- bae/src/page/Home/header/header.jsx
+++ bae/src/page/Home/header/header.jsx
@@ -1,26 +1,34 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Menu from "../../../layout/menu/menu";
 import styles from "./header.module.scss";
 import videoSrc from "../../../../src/assets/video/cay-44749.mp4";
+
 const Header = ({ user }) => {
   return (
     <div className={styles.main}>
       <Menu user={user} />
-      {/* Slider */}
       <div className={styles.slider}>
         <video autoPlay muted loop className={styles.video}>
           <source src={videoSrc} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-
         <div className={styles.content}>
           <div className={styles.sliderInfo}>
-            <p className={styles.sliderPhara}>Thu hồi pin cũ</p>
-            <p className={styles.sliderPhara}>Bảo vệ Trái Đất xanh</p>
+            <h1 className={styles.sliderPhara}>
+              <span className={styles.animatedText}>Thu gom pin cũ- </span>
+              <span className={styles.animatedText2}>bảo vệ môi trường sống</span>
+            </h1>
             <span className={styles.sliderMessage}>
-              - Hãy cùng chúng tôi làm những điều này nhé! -
+              <span className={styles.animatedText3}>
+                Chung tay hôm nay – Xanh mãi ngày mai!
+              </span>
             </span>
-            <div className={styles.liquid_glass} onClick={() => {window.location.href = "/dashboard"}}>
+            <div
+              className={styles.liquid_glass}
+              onClick={() => {
+                window.location.href = "/dashboard";
+              }}
+            >
               <a href="/dashboard" className={styles.sliderBtn}>
                 BẮT ĐẦU <i className="fa-solid fa-chevron-right"></i>
               </a>
